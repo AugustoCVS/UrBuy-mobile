401c9b416accef0dc4e6cf70c33b783485f59455
AugustoCVS <guty.vertolis@gmail.com>
2023-10-18 19:14:40 -0300

    build: install typescript

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View >
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
