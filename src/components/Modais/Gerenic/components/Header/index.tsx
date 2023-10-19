389e03bc3ec7a7071be2ba52df85ce2cbf000b88
AugustoCVS <guty.vertolis@gmail.com>
2023-10-18 22:54:18 -0300

    feat: create genericModalHeader

---
+++ src/components/Modais/Gerenic/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Pressable, View, Text} from 'react-native';
+import {Center } from 'native-base';
+import { AntDesign } from '@expo/vector-icons';
+
+
+export const GenericModalHeader = ({title, onClose}) => {
+  return (
+    <Center
+      h="60px"
+      borderTopLeftRadius="lg"
+      borderTopRightRadius="lg"
+      bg="bg-white">
+      {title && (
+        <View className="flex items-center justify-center text-center text-lg">
+         <Text>{title}</Text>
+        </View>
+      )}
+
+      <Pressable onPress={onClose} className="absolute right-4 top-4">
+        <AntDesign name="close" size={24} color="black"/>
+      </Pressable>
+    </Center>
+  );
+};
+
---
+++ src/components/Modais/Gerenic/components/Header/types.ts
@@ -0,0 +1,5 @@
+export interface HeaderProps {
+    title?: string;
+    onClose?: () => void;
+  }
+  
