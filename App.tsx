d664c727e81d4aafd132a760f2fdd02010b3a342
AugustoCVS <guty.vertolis@gmail.com>
2023-10-18 22:53:46 -0300

    feat: wrapped the main Component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import React from 'react';
 import StackComponent from './src/routes/stack';
+import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 
 function App(): JSX.Element {
-  return <StackComponent />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }} >
+      <NativeBaseProvider>
+        <StatusBar style="light" />
+        <StackComponent />
+      </NativeBaseProvider>
+    </GestureHandlerRootView>
+  );
 }
 
 export default App;
