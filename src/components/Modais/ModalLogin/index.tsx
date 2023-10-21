951bb845a316fc00fb82578b2fda6d08a6a01a48
AugustoCVS <guty.vertolis@gmail.com>
2023-10-21 01:10:39 -0300

    refactor: import Input component

--- src/components/Modais/ModalLogin/index.tsx
+++ src/components/Modais/ModalLogin/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Entypo } from "@expo/vector-icons";
 
 import * as T from "./types";
 import { Modal } from "src/components/Modais/Gerenic";
-import { Pressable, TextInput, View, Text } from "react-native";
+import { Pressable, View, Text } from "react-native";
 import { Button } from "src/components/Button";
 import { useModalLogin } from "./hook";
+import { Input } from "src/components/Input";
 
 export const ModalLogin: React.FC<T.ModalLoginProps> = ({ modalRef }) => {
-  const {states, actions} = useModalLogin()
+  const { states, actions } = useModalLogin();
 
   const onClose = () => {
     modalRef.current.close();
@@ -27,35 +28,41 @@ export const ModalLogin: React.FC<T.ModalLoginProps> = ({ modalRef }) => {
         </View>
       }
     >
-      <View className="flex flex-col gap-6 mt-4">
-        <TextInput
+      <View className="flex flex-col mt-4 w-80">
+        <Input
           placeholder="E-mail"
-          className="w-80 h-16 flex justify-start p-4 bg-gray-100 rounded-xl border border-solid border-blue-100"
           value={states.email}
-          onChangeText={(email) => {actions.setEmail(email)}}
+          onChangeText={(email) => {
+            actions.setEmail(email);
+          }}
         />
 
-      <View className="flex flex-row items-center">
-        <View className="w-80">
-          <TextInput
+        <View className="flex flex-row items-center">
+          <Input
             placeholder="Senha"
-            className="w-80 h-16 flex justify-start p-4 bg-gray-100 rounded-xl border border-solid border-blue-100"
             value={states.password}
-            onChangeText={(password) => {actions.setPassword(password)}}
+            onChangeText={(password) => {
+              actions.setPassword(password);
+            }}
             secureTextEntry={states.secureTextEntry}
           />
-          <Text className="text-gray-600 text-xs">A senha deve conter uma letras maíusculas e minúsculas, numeros e um caractere aespecial</Text>
+
+          <Pressable
+            className="absolute right-4 top-3"
+            onPress={actions.showPassword}
+          >
+            {states.secureTextEntry ? (
+              <Entypo name="eye-with-line" size={24} color="#B3B3B3" />
+            ) : (
+              <AntDesign name="eyeo" size={24} color="#B3B3B3" />
+            )}
+          </Pressable>
         </View>
-
-        <Pressable className="absolute right-4 top-5"
-        onPress={actions.showPassword}>
-          <AntDesign name="eyeo" size={24} color="#B3B3B3" />
-        </Pressable>
-      </View>
       </View>
 
-      <Button className="bg-green-100 flex items-center justify-center py-4 rounded-xl mt-16 w-80"
-      onPress={actions.handleSubmit}
+      <Button
+        className="bg-green-100 flex items-center justify-center py-4 rounded-xl mt-12 w-80"
+        onPress={actions.handleSubmit}
       >
         <Text className="text-white font-bold text-xl">Continuar</Text>
       </Button>
