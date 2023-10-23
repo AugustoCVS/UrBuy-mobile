43c669d60cfb850e8acbb31938d438cca37fb9ed
AugustoCVS <guty.vertolis@gmail.com>
2023-10-22 22:35:34 -0300

    feat: create yup validation Schema

--- src/components/Modais/ModalRegister/types.ts
+++ src/components/Modais/ModalRegister/types.ts
@@ -1,19 +1,11 @@
 import { RefObject } from "react";
 import {Modalize} from 'react-native-modalize';
+import * as yup from 'yup';
+
+import * as U from './utils'
 
 export type ModalRegisterProps = {
     modalRef: RefObject<Modalize>;
 }
 
-export type FormDataProps = {
-    companyName: string;
-    cnpj: string;
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    cep: string;
-    street: string;
-    isBuyer: boolean;
-    isSupplier: boolean;
-}
+export type useRegisterProps = yup.InferType<typeof U.signUpSchema>
