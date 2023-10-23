68b4d18d8ab6b13754abf7d0c5567fd6ddc4e39a
AugustoCVS <guty.vertolis@gmail.com>
2023-10-22 22:34:54 -0300

    feat: create handleSignUp

--- src/components/Modais/ModalRegister/hook.ts
+++ src/components/Modais/ModalRegister/hook.ts
@@ -1,21 +1,37 @@
-import { useToast} from 'native-base'
+import { useState } from "react";
+import { useToast } from "native-base";
+
+import * as T from "./types";
+import * as U from "./utils";
 
 export const useModalRegister = () => {
+  const toast = useToast();
 
-    const toast = useToast()
-    
-    const showErrorToast = (title: string): void => {
-        toast.show({
-            title: title,
-            duration: 3000,
-            bgColor: "red.500",
-            placement: "top",
-        })
-    }
+  const showErrorToast = (title: string): void => {
+    toast.show({
+      title: title,
+      duration: 3000,
+      bgColor: "red.500",
+      placement: "top",
+    });
+  };
 
-    return {
-        actions: {
-            showErrorToast
-        }
+  const handleSignUp = async (FormData: T.useRegisterProps): Promise<void> => {
+    try {
+      await U.signUpSchema.validate(FormData, { abortEarly: false });
+      // await api.post('/users', FormData);
+      // modalRef.current?.close();
+      // navigation.navigate('Login');
+      console.log(FormData);
+    } catch (error) {
+      showErrorToast("Erro ao cadastrar usuário!");
     }
-}
+  };
+
+  return {
+    states: {},
+    actions: {
+      handleSignUp,
+    },
+  };
+};
