1eb48c06ff41be3a7c241eee8dca23233120eb72
AugustoCVS <guty.vertolis@gmail.com>
2023-10-23 00:36:06 -0300

    refactor: change to map

--- src/components/Modais/ModalRegister/index.tsx
+++ src/components/Modais/ModalRegister/index.tsx
@@ -7,11 +7,11 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Modal } from "src/components/Modais/Gerenic";
 import { Input } from "src/components/Input";
 import { Button } from "src/components/Button";
-import { Checkbox } from "src/components/Checkbox";
 import { useModalRegister } from "./hook";
 
 import * as T from "./types";
 import * as U from "./utils";
+import { DropDown } from "src/components/DropDown";
 
 export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
   const { states, actions } = useModalRegister();
@@ -41,110 +41,33 @@ export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
       }
     >
       <ScrollView
-        className="flex flex-col mt-4"
+        className="flex flex-col mt-4 w-80"
         style={{ maxHeight: 400 }}
         showsVerticalScrollIndicator={false}
       >
-        <Controller
-          control={control}
-          name="companyName"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="Nome da Empresa"
-              onChangeText={onChange}
-              errorMessage={errors.companyName?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="cnpj"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="CNPJ"
-              onChangeText={onChange}
-              errorMessage={errors.cnpj?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="email"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="E-mail"
-              onChangeText={onChange}
-              errorMessage={errors.email?.message}
-            />
-          )}
-        />
-
-        <View className="w-80 mb-4">
+        {states.formFields.map((item) => (
           <Controller
+            key={item.name}
             control={control}
-            name="password"
+            name={item.name as keyof T.useRegisterProps}
             render={({ field: { onChange } }) => (
-              <Input
-                placeholder="Senha"
-                onChangeText={onChange}
-                errorMessage={errors.password?.message}
-              />
+              <>
+                <Input
+                  placeholder={item.placeholder}
+                  onChangeText={onChange}
+                  errorMessage={errors[item.name]?.message}
+                />
+                {item.name === "password" && (
+                  <Text className="text-gray-600 text-xs px-4 mb-2">
+                    A senha deve conter letras maiúsculas e minúsculas, números
+                    e um caractere especial.
+                  </Text>
+                )}
+              </>
             )}
           />
-          <Text className="text-gray-600 text-xs px-4">
-            A senha deve conter letras maíusculas e minúsculas, numeros e um
-            caractere aespecial
-          </Text>
-        </View>
-
-        <Controller
-          control={control}
-          name="confirmPassword"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="Confirmar Senha"
-              onChangeText={onChange}
-              errorMessage={errors.confirmPassword?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="cep"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="Cep"
-              onChangeText={onChange}
-              errorMessage={errors.cep?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="street"
-          render={({ field: { onChange } }) => (
-            <Input
-              placeholder="Rua"
-              onChangeText={onChange}
-              errorMessage={errors.street?.message}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="complement"
-          render={({ field: { onChange } }) => (
-            <Input placeholder="Complemento" onChangeText={onChange} />
-          )}
-        />
-
+        ))}
       </ScrollView>
-
       <Button
         className="bg-green-100 flex items-center justify-center py-4 rounded-xl mt-16 w-80"
         onPress={handleSubmit(actions.handleSignUp)}
