37c9a6637597454ca26c8bb090b01049f2faf4a9
AugustoCVS <guty.vertolis@gmail.com>
2023-10-22 22:35:55 -0300

    feat: add Inputs

--- src/components/Modais/ModalRegister/index.tsx
+++ src/components/Modais/ModalRegister/index.tsx
@@ -2,25 +2,30 @@ import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { View, Text, Pressable, ScrollView } from "react-native";
 import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-import * as T from "./types";
 import { Modal } from "src/components/Modais/Gerenic";
 import { Input } from "src/components/Input";
 import { Button } from "src/components/Button";
 import { Checkbox } from "src/components/Checkbox";
 import { useModalRegister } from "./hook";
 
+import * as T from "./types";
+import * as U from "./utils";
+
 export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
-  const { actions } = useModalRegister();
-  const { control, handleSubmit, formState: { errors } } = useForm<T.FormDataProps>();
+  const { states, actions } = useModalRegister();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<T.useRegisterProps>({
+    resolver: yupResolver(U.signUpSchema),
+  });
 
   const onClose = (): void => {
     modalRef.current.close();
   };
-
-  const handleSignUp = (data: T.FormDataProps): void => {
-    console.log(data);
-  }
 
   return (
     <Modal
@@ -43,31 +48,36 @@ export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
         <Controller
           control={control}
           name="companyName"
-          rules = {{
-            required: 'O nome da empresa é obrigatório'
-          }}
           render={({ field: { onChange } }) => (
-            <Input 
-              placeholder="Nome da Empresa" 
+            <Input
+              placeholder="Nome da Empresa"
               onChangeText={onChange}
               errorMessage={errors.companyName?.message}
-              />
+            />
           )}
         />
 
         <Controller
           control={control}
           name="cnpj"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input placeholder="CNPJ" onChangeText={onChange} />
+          render={({ field: { onChange } }) => (
+            <Input
+              placeholder="CNPJ"
+              onChangeText={onChange}
+              errorMessage={errors.cnpj?.message}
+            />
           )}
         />
 
         <Controller
           control={control}
           name="email"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input placeholder="E-mail" onChangeText={onChange} />
+          render={({ field: { onChange } }) => (
+            <Input
+              placeholder="E-mail"
+              onChangeText={onChange}
+              errorMessage={errors.email?.message}
+            />
           )}
         />
 
@@ -75,8 +85,12 @@ export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
           <Controller
             control={control}
             name="password"
-            render={({ field: { onChange, onBlur, value } }) => (
-              <Input placeholder="Senha" onChangeText={onChange} />
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
             )}
           />
           <Text className="text-gray-600 text-xs px-4">
@@ -88,47 +102,52 @@ export const ModalRegister: React.FC<T.ModalRegisterProps> = ({ modalRef }) => {
         <Controller
           control={control}
           name="confirmPassword"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input placeholder="Confirmar Senha" onChangeText={onChange} />
+          render={({ field: { onChange } }) => (
+            <Input
+              placeholder="Confirmar Senha"
+              onChangeText={onChange}
+              errorMessage={errors.confirmPassword?.message}
+            />
           )}
         />
 
         <Controller
           control={control}
           name="cep"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input placeholder="Cep" onChangeText={onChange} />
+          render={({ field: { onChange } }) => (
+            <Input
+              placeholder="Cep"
+              onChangeText={onChange}
+              errorMessage={errors.cep?.message}
+            />
           )}
         />
 
         <Controller
           control={control}
           name="street"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Input placeholder="Rua" onChangeText={onChange} />
+          render={({ field: { onChange } }) => (
+            <Input
+              placeholder="Rua"
+              onChangeText={onChange}
+              errorMessage={errors.street?.message}
+            />
           )}
         />
 
         <Controller
           control={control}
-          name="isBuyer"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Checkbox label="Comprador" checked={false} onPress={() => {}} />
+          name="complement"
+          render={({ field: { onChange } }) => (
+            <Input placeholder="Complemento" onChangeText={onChange} />
           )}
         />
 
-        <Controller
-          control={control}
-          name="isSupplier"
-          render={({ field: { onChange, onBlur, value } }) => (
-            <Checkbox label="Fornecedor" checked={false} onPress={() => {}} />
-          )}
-        />
-        
       </ScrollView>
 
-      <Button className="bg-green-100 flex items-center justify-center py-4 rounded-xl mt-16 w-80"
-      onPress={handleSubmit(handleSignUp)}
+      <Button
+        className="bg-green-100 flex items-center justify-center py-4 rounded-xl mt-16 w-80"
+        onPress={handleSubmit(actions.handleSignUp)}
       >
         <Text className="text-white font-bold text-xl">Cadastrar</Text>
       </Button>
