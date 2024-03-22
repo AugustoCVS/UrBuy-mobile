4acff5f1d83bcf3d641286b863546fb27027e9ae
AugustoCVS <guty.vertolis@gmail.com>
2024-03-22 18:28:49 -0300

    refactor: code format

--- src/screens/Dashboard/components/Products/components/ProductCard/index.tsx
+++ src/screens/Dashboard/components/Products/components/ProductCard/index.tsx
@@ -1,46 +1,52 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
-import { Feather} from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 
-import { Button } from 'src/components/Button';
-import { useProductCard } from './hook';
+import { Button } from "src/components/Button";
+import { useProductCard } from "./hook";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const ProductScreen: React.FC = () => {
-  const {states} = useProductCard();
+  const { states } = useProductCard();
 
   return (
     <View className="flex-1 items-center bg-gray-100 p-4 justify-between">
-      <View className='bg-gray-600 h-52 w-full rounded-md' />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="bg-gray-600 h-52 w-full rounded-md" />
 
+        <View className="bg-white p-8 rounded-lg shadow-md w-full mt-8">
+          <Text className="text-3xl font-bold mb-4">{states.product.name}</Text>
+          <Text className="text-lg mb-2">
+            Descrição: {states.product.description}
+          </Text>
+          <Text className="text-lg mb-2">Preço: R${states.product.price}</Text>
+          <Text className="text-lg mb-2">
+            Quantidade Disponível: {states.product.quantity}
+          </Text>
 
-      <View className="bg-white p-8 rounded-lg shadow-md w-11/12">
-        <Text className="text-3xl font-bold mb-4">{states.product.name}</Text>
-        <Text className="text-lg mb-2">Descrição: {states.product.description}</Text>
-        <Text className="text-lg mb-2">Preço: R${states.product.price}</Text>
-        <Text className="text-lg mb-2">Quantidade Disponível: {states.product.quantity}</Text>
-
-        <View className="mt-6">
-          <Text className="text-xl font-bold mb-2">Informações Adicionais:</Text>
-          <Text className="text-lg">Marca: XYZ</Text>
-          <Text className="text-lg">Categoria: Eletrônicos</Text>
-          <Text className="text-lg">Peso: 500g</Text>
+          <View className="mt-6">
+            <Text className="text-xl font-bold mb-2">
+              Informações Adicionais:
+            </Text>
+            <Text className="text-lg">Marca: XYZ</Text>
+            <Text className="text-lg">Categoria: Eletrônicos</Text>
+            <Text className="text-lg">Peso: 500g</Text>
+          </View>
         </View>
-      </View>
 
-      <View className="flex flex-row items-center justify-center mb-8">
-        <Button className="h-14 w-3/4 flex flex-row items-center justify-center bg-green-100 rounded-md"
-          
-        >
-          <Feather
-            name="shopping-bag"
-            size={14}
-            color="white"
-            style={{ padding: 4 }}
-          />
-          <Text className="text-lg text-white">Comprar</Text>
-        </Button>
+        <View className="flex flex-row items-center justify-center my-8">
+          <Button className="h-14 w-full flex flex-row items-center justify-center bg-green-100 rounded-md">
+            <Feather
+              name="shopping-bag"
+              size={14}
+              color="white"
+              style={{ padding: 4 }}
+            />
+            <Text className="text-lg text-white ml-4">Comprar</Text>
+          </Button>
         </View>
+      </ScrollView>
     </View>
   );
 };
