949dd4adaf3291d58563aab87be69faa21e17502
AugustoCVS <guty.vertolis@gmail.com>
2024-03-22 11:51:12 -0300

    feat: add providers

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import 'react-native-gesture-handler';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import React from 'react';
-import StackComponent from './src/routes/stack.routes';
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React from "react";
+import StackComponent from "./src/routes/stack.routes";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import { AuthProvider } from "src/context/AuthContext/auth.context";
+import { ProductProvider } from "src/context/ProductContext/product.context";
 
 function App(): JSX.Element {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }} >
-      <NativeBaseProvider>
-        <StatusBar style="light" />
-        <StackComponent />
-      </NativeBaseProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <ProductProvider>
+          <NativeBaseProvider>
+            <StatusBar style="light" />
+            <StackComponent />
+          </NativeBaseProvider>
+        </ProductProvider>
+      </AuthProvider>
     </GestureHandlerRootView>
   );
 }
 
 export default App;
-
