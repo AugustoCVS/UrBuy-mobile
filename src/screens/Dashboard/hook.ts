65c73495526d006d083db7afbd43069d733049c4
AugustoCVS <guty.vertolis@gmail.com>
2024-03-26 15:49:22 -0300

    feat: add navigation to products list

--- src/screens/Dashboard/hook.ts
+++ src/screens/Dashboard/hook.ts
@@ -1,22 +1,36 @@
-import { useContext } from "react"
-import { ProductContext, ProductProps } from "src/context/ProductContext/product.context"
+import { useContext, useState } from "react";
+import {
+  ProductContext,
+  ProductProps,
+} from "src/context/ProductContext/product.context";
 
-import { useNavigation } from "@react-navigation/native"
-import { StackTypes } from "src/routes/stack.routes"
+import { useNavigation } from "@react-navigation/native";
+import { StackTypes } from "src/routes/stack.routes";
+import { TabTypes } from "src/routes/tab.routes";
 
 export const useDashboard = () => {
-  const {actions} = useContext(ProductContext)
+  const { actions } = useContext(ProductContext);
   const navigation = useNavigation<StackTypes>();
-  
-  const handleNavigateToProduct = ({product}: {product: ProductProps}): void => {
-    actions.setProduct(product)
-    navigation.navigate("SingleProduct")
+  const tabNavigation = useNavigation<TabTypes>()
+
+  const handleNavigateToProductList = (productType: string): void => {
+    actions.setProductType(productType);
+    tabNavigation.navigate("Products")
   }
+
+  const handleNavigateToProduct = ({
+    product,
+  }: {
+    product: ProductProps;
+  }): void => {
+    actions.setProduct(product);
+    navigation.navigate("SingleProduct");
+  };
 
   return {
     actions: {
-      handleNavigateToProduct
-    }
-  }
-
-}
+      handleNavigateToProduct,
+      handleNavigateToProductList,
+    },
+  };
+};
