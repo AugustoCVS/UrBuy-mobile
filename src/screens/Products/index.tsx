a6ea5de72dd79eb49258b3beb5713e0f88cb7a30
AugustoCVS <guty.vertolis@gmail.com>
2024-03-27 15:31:14 -0300

    feat: implementing searchBar

--- src/screens/Products/index.tsx
+++ src/screens/Products/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native"
+import React from "react";
+import { View, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import { SearchBar } from "src/components/SearchBar";
+import { useProducts } from "./hook";
 
 export const ProductsAvailable: React.FC = () => {
+  const { states, actions } = useProducts();
 
-    return(
-        <View className="flex items-center justify-center h-full" >
-            <Text>Products</Text>
-        </View>
-    )
+  const ICON = <AntDesign name="search1" size={24} color="#749E50" />;
 
-}
+  return (
+    <View className={`flex mt-16 items-center h-full w-full`}>
+      <SearchBar
+        placeholder="Busque um produto"
+        onChangeValue={actions.onChangeText}
+        value={states.searchValue}
+        icon={ICON}
+      />
+    </View>
+  );
+};
