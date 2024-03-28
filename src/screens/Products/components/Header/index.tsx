e0c57ad29c1c0defc5aba3bb9aa66c0d39bf3993
AugustoCVS <guty.vertolis@gmail.com>
2024-03-28 18:11:47 -0300

    refactor: change color

--- src/screens/Products/components/Header/index.tsx
+++ src/screens/Products/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { AntDesign, EvilIcons } from "@expo/vector-icons";
 import { HeaderProps } from "./types";
 
 export const Header: React.FC<HeaderProps> = ({ onChangeText, value }) => {
-  const ICON = <AntDesign name="search1" size={24} color="#749E50" />;
+  const ICON = <AntDesign name="search1" size={24} color="#173042FC" />;
 
   return (
     <View className="w-full h-40 bg-blue-100 flex flex-row items-center justify-center px-4 shadow-sm shadow-gray-600">
