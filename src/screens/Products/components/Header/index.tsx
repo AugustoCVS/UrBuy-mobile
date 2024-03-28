fcb8bc68d28b92b00d176dfbfb91e4d59bf91e6d
AugustoCVS <guty.vertolis@gmail.com>
2024-03-28 17:29:39 -0300

    refactor:  change style

--- src/screens/Products/components/Header/index.tsx
+++ src/screens/Products/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import { SearchBar } from "src/components/SearchBar";
 
 import { AntDesign, EvilIcons } from "@expo/vector-icons";
@@ -8,22 +8,24 @@ export const Header: React.FC<HeaderProps> = ({ onChangeText, value }) => {
   const ICON = <AntDesign name="search1" size={24} color="#749E50" />;
 
   return (
-    <View className="flex flex-row items-center justify-center">
-      <SearchBar
-        placeholder="Busque um produto"
-        onChangeValue={onChangeText}
-        value={value}
-        icon={ICON}
-      />
-
-      <TouchableOpacity>
-        <EvilIcons
-          name="cart"
-          size={36}
-          color="#173042FC"
-          style={{ paddingLeft: 8 }}
+    <View className="w-full h-40 bg-blue-100 flex flex-row items-center justify-center px-4 shadow-sm shadow-gray-600">
+      <View className="mt-12 flex flex-row items-center">
+        <SearchBar
+          placeholder="Busque um produto"
+          onChangeValue={onChangeText}
+          value={value}
+          icon={ICON}
         />
-      </TouchableOpacity>
+
+        <TouchableOpacity>
+          <EvilIcons
+            name="cart"
+            size={36}
+            color="white"
+            style={{ paddingLeft: 8 }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
