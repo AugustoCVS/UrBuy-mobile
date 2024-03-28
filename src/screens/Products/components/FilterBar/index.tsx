eb244751a96bf3d7258ce84d827fe4a29acd6ae7
AugustoCVS <guty.vertolis@gmail.com>
2024-03-28 17:29:20 -0300

    feat: add styling and logic to change icons color

--- src/screens/Products/components/FilterBar/index.tsx
+++ src/screens/Products/components/FilterBar/index.tsx
@@ -2,8 +2,20 @@ import { ProductsTypesList } from "src/components/ProductsTypesList";
 
 import * as U from "./utils";
 import { FlatList } from "react-native";
+import { useContext } from "react";
+import { ProductContext } from "src/context/ProductContext/product.context";
 
 export const FilterBar: React.FC = () => {
+  const { states, actions } = useContext(ProductContext);
+
+  const handleChangeProductTypeColor = ({ category }: { category: string }) => {
+    if (states.category === category) {
+      return "bg-green-100";
+    }
+
+    return "bg-blue-100";
+  };
+
   return (
     <FlatList
       data={U.TypesList}
@@ -12,12 +24,17 @@ export const FilterBar: React.FC = () => {
         <ProductsTypesList
           key={index}
           category={item.category}
-          onPress={() => {}}
+          styling={`${handleChangeProductTypeColor({
+            category: item.category,
+          })} w-24 h-12 rounded-full justify-center items-center shadow-sm shadow-gray-700 mt-4`}
+          iconSize={36}
+          iconColor="white"
+          onPress={() => actions.setCategory(item.category)}
         />
       )}
       horizontal
       showsHorizontalScrollIndicator={false}
-      contentContainerStyle={{ gap: 28, padding: 8 }}
+      contentContainerStyle={{ gap: 28, padding: 8, height: 90}}
     />
   );
 };
