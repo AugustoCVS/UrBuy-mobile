1eb0d2d1ca7240aa860492fd8163b3e5754c3cdc
AugustoCVS <guty.vertolis@gmail.com>
2024-03-28 18:02:17 -0300

    feat: create products list

---
+++ src/screens/Products/utils.ts
@@ -0,0 +1,41 @@
+export const ProductList = [
+  {
+    id: "0",
+    name: "RTX 3090",
+    category: "GPU",
+    price: 1599.99,
+    amount: 15,
+    img: [
+      "https://img.olx.com.br/images/40/409309596298995.webp",
+      "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-600-p@2x.png",
+    ],
+    brand: "Nvidia",
+    description: "Placa de vídeo RTX 3090 da NVIDIA, pesando 1.4kg, com 24GB de memória GDDR6X e 384-bit.",
+  },
+  {
+    id: "1",
+    name: "Ryzen 9 5900X",
+    category: "CPU",
+    price: 599.99,
+    amount: 25,
+    img: [
+      "https://i.zst.com.br/thumbs/12/18/16/1949930390.jpg",
+      "https://i.zst.com.br/thumbs/12/18/16/1949930390.jpg",
+    ],
+    brand: "Nvidia",
+    description: "1.4kg",
+  },
+  {
+    id: "3",
+    name: "iPhone 13 Pro",
+    category: "Smartphone",
+    price: 1099.99,
+    amount: 12,
+    img: [
+      "https://s3-sa-east-1.amazonaws.com/loja2/7eb17f42bdccf102fc71d9ec353940b7.jpg",
+      "https://s3-sa-east-1.amazonaws.com/loja2/7eb17f42bdccf102fc71d9ec353940b7.jpg",
+    ],
+    brand: "Nvidia",
+    description: "1.4kg",
+  },
+];
