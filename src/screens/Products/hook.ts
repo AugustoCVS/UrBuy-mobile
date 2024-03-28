dbdec5ab8b644c2f1b96baca2f1d880669cabb6e
AugustoCVS <guty.vertolis@gmail.com>
2024-03-28 18:03:06 -0300

    feat: add handleNavigateToProduct

--- src/screens/Products/hook.ts
+++ src/screens/Products/hook.ts
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import {
+  ProductContext,
+  ProductProps,
+} from "src/context/ProductContext/product.context";
+import { StackTypes } from "src/routes/stack.routes";
 
 export const useProducts = () => {
+  const { actions } = useContext(ProductContext);
+  const navigation = useNavigation<StackTypes>();
+
   const [searchValue, setSearchValue] = useState<string>("");
 
   const onChangeText = (value: string) => {
     setSearchValue(value);
+  };
+
+  const handleNavigateToProduct = ({
+    product,
+  }: {
+    product: ProductProps;
+  }): void => {
+    actions.setProduct(product);
+    navigation.navigate("SingleProduct");
   };
 
   return {
@@ -13,6 +31,7 @@ export const useProducts = () => {
     },
     actions: {
       onChangeText,
+      handleNavigateToProduct,
     },
   };
 };
