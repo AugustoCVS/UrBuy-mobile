5dd94cfe046db0bb3611b0b3896c6fe37aa4b055
AugustoCVS <guty.vertolis@gmail.com>
2024-05-17 17:25:16 -0300

    feat: add default values on getProducts props

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,7 +1,7 @@
 import { api } from "./api";
 import { ProductRequest, ProductResponse } from "./interfaces/product";
 export const ProductService = {
-  getProducts: async ({ name, category, page, size }: ProductRequest) => {
+  getProducts: async ({ name = "", category = "", page = 0, size = 20 }: ProductRequest) => {
     const res = await api.get<ProductResponse>(
       `/product?category=${category}&name=${name}&page=${page}&size=${size}'`
     );
