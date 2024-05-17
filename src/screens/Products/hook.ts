d1d3fa12529bb7fd16b1d9edb713cc62ef929342
AugustoCVS <guty.vertolis@gmail.com>
2024-05-17 16:32:54 -0300

    feat: add handleGetProducts

--- src/screens/Products/hook.ts
+++ src/screens/Products/hook.ts
@@ -1,16 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
-import { useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import {
   ProductContext,
   ProductProps,
 } from "src/context/ProductContext/product.context";
 import { StackTypes } from "src/routes/stack.routes";
+import { IProduct } from "src/services/interfaces/product";
+import { ProductService } from "src/services/product";
 
 export const useProducts = () => {
-  const { actions } = useContext(ProductContext);
+  const { states, actions } = useContext(ProductContext);
   const navigation = useNavigation<StackTypes>();
 
+  const [loading, setLoading] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>("");
+  const [products, setProducts] = useState<IProduct[]>([])
 
   const onChangeText = (value: string) => {
     setSearchValue(value);
@@ -25,9 +29,34 @@ export const useProducts = () => {
     navigation.navigate("SingleProduct");
   };
 
+  const handleGetProduct = useCallback(async() => {
+    setLoading(true);
+    await ProductService.getProducts({
+      category: states.category,
+      name: searchValue,
+      page: 0,
+      size: 20,
+    })
+    .then((res) => {
+      setProducts(res.content)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [searchValue, states.category])
+
+
+  useEffect(() => {
+    handleGetProduct()
+  }, [])
+
   return {
     states: {
       searchValue,
+      products,
     },
     actions: {
       onChangeText,
