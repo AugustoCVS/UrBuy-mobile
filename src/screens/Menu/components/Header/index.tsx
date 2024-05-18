d9f04c61713d5d5caa56fb842289889122bcddd7
AugustoCVS <guty.vertolis@gmail.com>
2024-05-18 13:54:37 -0300

    feat: create Header

---
+++ src/screens/Menu/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+import { HeaderProps } from "./types";
+
+export const Header: React.FC<HeaderProps> = ({ name }) => {
+  return (
+    <View className="flex items-center mb-6">
+      <View className="w-28 h-28 rounded-full bg-gray-600 flex items-center justify-center mb-8">
+        <FontAwesome name="camera" size={40} color="white" />
+      </View>
+      <View className="ml-4">
+        <Text className="text-xl font-bold">{name}</Text>
+      </View>
+    </View>
+  );
+};
