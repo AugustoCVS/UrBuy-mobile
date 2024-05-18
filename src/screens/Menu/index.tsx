9bc7f35ddc0ff0dedb8b07c85c093741f82f4e85
AugustoCVS <guty.vertolis@gmail.com>
2024-05-18 14:01:08 -0300

    feat: add hook

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -1,38 +1,23 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { MenuItem } from "./components/MenuItem";
+import { Header } from "./components/Header";
+import { useMenu } from "./hook";
 
 export const Menu: React.FC = () => {
+  const { states } = useMenu();
+
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
       <View className="bg-white p-8 rounded-lg shadow-md w-11/12">
-        <View className="flex items-center mb-6">
-          <View className='w-28 h-28 rounded-full bg-gray-600 flex items-center justify-center mb-8'>
-          <FontAwesome name="camera" size={40} color="white" />
-          </View>
-          <View className="ml-4">
-            <Text className="text-xl font-bold">Nome do Usuário</Text>
-            <Text className="text-gray-600">Cargo / Descrição do Usuário</Text>
-          </View>
-        </View>
+        <Header name={states.userName} />
 
         <View className="border-t border-gray-300 pt-4">
-          <MenuItem label="Perfil" iconName="user-circle" />
-          <MenuItem label="Configurações" iconName="cog" />
+          {states.userInfo.map((info) => (
+            <MenuItem key={info.label} label={info.label} value={info.value} />
+          ))}
         </View>
       </View>
-    </View>
-  );
-};
-
-const MenuItem = ({ label, iconName }: { label: string; iconName: string }) => {
-  return (
-    <View className="flex flex-row items-center mb-3">
-      <Image
-        source={{ uri: `ICON_URL_${iconName}` }}
-        className="w-8 h-8 mr-3"
-      />
-      <Text className="text-lg">{label}</Text>
     </View>
   );
 };
