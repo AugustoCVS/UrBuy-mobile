bb76d00ba28e5b35c123a63339992d8fa9855d15
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 13:06:34 -0300

    feat: create Header for update modal info

---
+++ src/screens/Menu/components/ModalUpdateInfo/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderProps } from "./types";
+
+import { AntDesign } from "@expo/vector-icons";
+import { Pressable, Text, View } from "react-native";
+
+export const Header: React.FC<HeaderProps> = ({ onClose }) => {
+  return (
+    <View className="flex justify-center items-center mt-4">
+      <Text className="text-blue-100 font-bold text-2xl">Atualizar Dados</Text>
+
+      <Pressable onPress={onClose} className="absolute right-4 top-1">
+        <AntDesign name="close" size={26} color="black" />
+      </Pressable>
+    </View>
+  );
+};
