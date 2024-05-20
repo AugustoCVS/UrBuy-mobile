78e939e61aaa500bc762632d17aa73e6de85707a
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 20:17:57 -0300

    refactor: adjusts

--- src/components/ListEmtpy/components/CartEmpty/index.tsx
+++ src/components/ListEmtpy/components/CartEmpty/index.tsx
@@ -4,7 +4,7 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export const CartEmpty: React.FC = () => {
   return (
-    <View className="w-full flex flex-col items-center justify-center gap-8">
+    <View className="w-full flex flex-col items-center justify-center gap-8 mt-12">
       <MaterialCommunityIcons name="cart-remove" size={120} color="#BFBFBF" />
       <Text className="text-2xl font-bold text-gray-500">
         Seu carrinho está vazio
