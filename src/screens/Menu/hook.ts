9739d5c797c6791d868012a65b847390083f553f
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 12:38:54 -0300

    feat: create handleOpenUpdateModal

--- src/screens/Menu/hook.ts
+++ src/screens/Menu/hook.ts
@@ -1,17 +1,23 @@
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
+import { Modalize } from "react-native-modalize";
 import { AuthContext } from "src/context/AuthContext/auth.context";
 import { useMessage } from "src/utils/message";
 
 export const useMenu = () => {
   const { states } = useContext(AuthContext);
   const { showToast } = useMessage();
+  
+  const modalUpdateRef = useRef<Modalize>(null);
 
   const userName = states.user.name;
-
   const userInfo = [
     { label: "CNPJ", value: states.user.cnpj },
     { label: "Email", value: states.user.email },
   ];
+
+  const handleOpenUpdateModal = (): void => {
+    modalUpdateRef.current?.open();
+  }
 
   const handleChangePhoto = (): void => {
     showToast({
@@ -21,12 +27,16 @@ export const useMenu = () => {
   };
 
   return {
+    refs: {
+      modalUpdateRef,
+    },
     states: {
       userName,
       userInfo,
     },
     actions: {
       handleChangePhoto,
+      handleOpenUpdateModal,
     }
   };
 };
