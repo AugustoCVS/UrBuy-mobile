286e52d3d95d0c29fa870040a1f031694bbf0a1d
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 13:35:08 -0300

    feat: create renderInput for update user info

--- src/screens/Menu/components/ModalUpdateInfo/index.tsx
+++ src/screens/Menu/components/ModalUpdateInfo/index.tsx
@@ -1,14 +1,71 @@
 import { Modal } from "src/components/Modais/Gerenic";
 
-import { Text } from "react-native";
-import { ModalUpdateInfoProps } from "./types";
+import { Text, View } from "react-native";
+import { ModalUpdateInfoProps, formProps } from "./types";
+import { Header } from "./components/Header";
+import { useModalUpdateInfo } from "./hook";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { formFields, updateUserInfoSchema } from "./utils";
+import { Input } from "src/components/Input";
+import { Button } from "src/components/Button";
+import { ButtonSection } from "./components/ButtonSection";
 
 export const ModalUpdateInfo: React.FC<ModalUpdateInfoProps> = ({
   modalRef,
+  userInfo,
 }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formProps>({
+    resolver: yupResolver(updateUserInfoSchema),
+    values: {
+      name: userInfo.name,
+      email: userInfo.email,
+      cnpj: userInfo.cnpj,
+    },
+  });
+
+  const { states, actions } = useModalUpdateInfo({ modalRef, userInfo });
+
+  const renderInputs = () => {
+    return (
+      <View className="w-full flex items-center justify-center pt-8">
+        {formFields.map((field) => {
+          return (
+            <Controller
+              key={field.id}
+              control={control}
+              name={field.name as keyof formProps}
+              render={({ field: { onChange } }) => (
+                <Input
+                  placeholder={field.placeholder}
+                  onChangeText={onChange}
+                  errorMessage={errors[field.name]?.message}
+                />
+              )}
+            />
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
-    <Modal title="Teste" modalizeRef={modalRef}>
-      <Text>HELLO</Text>
+    <Modal
+      title="Teste"
+      modalizeRef={modalRef}
+      HeaderComponent={<Header onClose={actions.handleCloseModal} />}
+    >
+      <View className=" items-center py-4">
+        {renderInputs()}
+        <ButtonSection
+          handleUpdateUserInfo={handleSubmit(actions.handleUpdateUserInfo)}
+          loading={states.loading}
+        />
+      </View>
     </Modal>
   );
 };
