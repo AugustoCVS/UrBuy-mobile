d03afd794fa227cc5b4821cc0b4cf92f3f391677
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 12:38:25 -0300

    feat: add modal and button section

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -3,24 +3,35 @@ import { View, Text, Image } from "react-native";
 import { MenuItem } from "./components/MenuItem";
 import { Header } from "./components/Header";
 import { useMenu } from "./hook";
+import { ModalUpdateInfo } from "./components/ModalUpdateInfo";
+import { ButtonSection } from "./components/ButtonSection";
 
 export const Menu: React.FC = () => {
-  const { states, actions } = useMenu();
+  const { refs, states, actions } = useMenu();
 
   return (
-    <View className="flex-1 justify-center items-center bg-gray-100">
-      <View className="bg-white p-8 rounded-lg shadow-md w-11/12">
-        <Header
-          name={states.userName}
-          handleChangePhoto={actions.handleChangePhoto}
-        />
+    <>
+      <View className="flex-1 justify-center items-center bg-gray-100">
+        <View className="bg-white p-8 rounded-lg shadow-md w-11/12">
+          <Header
+            name={states.userName}
+            handleChangePhoto={actions.handleChangePhoto}
+          />
 
-        <View className="border-t border-gray-300 pt-4">
-          {states.userInfo.map((info) => (
-            <MenuItem key={info.label} label={info.label} value={info.value} />
-          ))}
+          <View className="border-t border-gray-300 pt-4">
+            {states.userInfo.map((info) => (
+              <MenuItem
+                key={info.label}
+                label={info.label}
+                value={info.value}
+              />
+            ))}
+          </View>
         </View>
+        <ButtonSection onPress={actions.handleOpenUpdateModal} />
       </View>
-    </View>
+
+      <ModalUpdateInfo modalRef={refs.modalUpdateRef} />
+    </>
   );
 };
