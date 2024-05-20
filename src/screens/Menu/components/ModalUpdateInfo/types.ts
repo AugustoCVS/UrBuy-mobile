fe1126f5e30aca511bf5ae40b236584867c23dd4
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 13:06:57 -0300

    feat: add userInfo on modal props

--- src/screens/Menu/components/ModalUpdateInfo/types.ts
+++ src/screens/Menu/components/ModalUpdateInfo/types.ts
@@ -1,6 +1,8 @@
 import { RefObject } from "react";
 import { Modalize } from "react-native-modalize";
+import { AuthContextProps } from "src/context/AuthContext/auth.context";
 
 export type ModalUpdateInfoProps = {
   modalRef: RefObject<Modalize>;
+  userInfo: AuthContextProps
 };
