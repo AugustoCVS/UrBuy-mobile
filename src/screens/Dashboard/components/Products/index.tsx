4f5eae3a7e161dc928ab380f667d4ed2268dc1b0
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 16:52:54 -0300

    chore: remove unused code

--- src/screens/Dashboard/components/Products/index.tsx
+++ src/screens/Dashboard/components/Products/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { View, TouchableOpacity } from "react-native";
 
 import * as T from "./types";
-import { useProducts } from "./hook";
 import { ProductName } from "./components/ProductName";
 import { ProductInfo } from "./components/ProductInfo";
 import { ProductImage } from "./components/ProductImage";
@@ -18,8 +17,6 @@ export const Products: React.FC<T.ProductsProps> = ({
   onPress,
   handleOpenModal,
 }) => {
-  const { actions } = useProducts();
-
   return (
     <TouchableOpacity onPress={onPress} key={id}>
       <View className="w-[350px] h-[200px] bg-blue-100 shadow shadow-gray-700 rounded-xl flex flex-row items-center justify-center mt-4 p-4">
