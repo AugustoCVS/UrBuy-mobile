507e5bd832490388064e42b5a6f5a272a02f8a2c
AugustoCVS <guty.vertolis@gmail.com>
2024-05-20 13:35:18 -0300

    feat: create formProps

--- src/screens/Menu/components/ModalUpdateInfo/types.ts
+++ src/screens/Menu/components/ModalUpdateInfo/types.ts
@@ -2,7 +2,12 @@ import { RefObject } from "react";
 import { Modalize } from "react-native-modalize";
 import { AuthContextProps } from "src/context/AuthContext/auth.context";
 
+import * as yup from "yup";
+import { updateUserInfoSchema } from "./utils";
+
 export type ModalUpdateInfoProps = {
   modalRef: RefObject<Modalize>;
   userInfo: AuthContextProps
 };
+
+export type formProps = yup.InferType<typeof updateUserInfoSchema>;
